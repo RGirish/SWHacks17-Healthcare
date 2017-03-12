b2454f06335af38baffaef2e3b87e8a1103b6a38
Girish Raman <rgirish1994@gmail.com>
2017-03-12 06:45:06 -0700

    Changed API END POINT

--- Girish/app/src/main/java/com/girish/raman/healthcare/utils/Constants.java
+++ Girish/app/src/main/java/com/girish/raman/healthcare/utils/Constants.java
@@ -1,9 +1,10 @@
 package com.girish.raman.healthcare.utils;
 
 public class Constants {
+    // public static final String API_END_POINT = "http://192.168.0.6:8080/"; //Girish
     // public static final String API_END_POINT = "http://192.168.0.8:8080/"; //Boss
-    public static final String API_END_POINT = "http://192.168.0.7:8080/"; //KP
-    // public static final String API_END_POINT = "http://10.152.114.246:8080/"; //K
+    // public static final String API_END_POINT = "http://192.168.0.7:8080/"; //KP
+    public static final String API_END_POINT = "http://10.114.188.118:8080/"; //K
     // public static final String API_END_POINT = "http://10.152.130.151:8080/";//P
     public static final String GETDIAGNOSIS_API_END_POINT = API_END_POINT + "getDiagnosis";
     public static final String GETDOCTORS_API_END_POINT = API_END_POINT + "getDoctorsAndInsurance";
